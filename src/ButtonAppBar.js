62268954fc79c7d682d9e17ca8add5f801c2467d
Leaf-WXC <523045770@qq.com>
2018-03-28 10:55:20 +0800

    实现所有功能

--- src/ButtonAppBar.js
+++ src/ButtonAppBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
@@ -7,6 +7,8 @@ import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import { MenuItem } from 'material-ui/Menu';
+import Drawer from 'material-ui/Drawer';
 
 const styles = {
   root: {
@@ -21,13 +23,22 @@ const styles = {
   },
 };
 
-function ButtonAppBar(props) {
-  const { classes } = props;
+class ButtonAppBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+  handleClose = () => this.setState({open: false});
+
+  render() {
+  const { classes } =this.props;
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu"  onClick={this.handleToggle}>
             <MenuIcon />
           </IconButton>
           <Typography variant="title" color="inherit" className={classes.flex}>
@@ -36,12 +47,17 @@ function ButtonAppBar(props) {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Drawer  open={this.state.open} onClose={this.handleToggle} >
+          <MenuItem> 功能1 </MenuItem>
+          <MenuItem> 功能2 </MenuItem>
+          <MenuItem> 功能3 </MenuItem>
+        </Drawer>
     </div>
   );
+}
 }
 
 ButtonAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-
 export default withStyles(styles)(ButtonAppBar);
