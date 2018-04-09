1ecd79e7a99a0e3c9a66e76cc449c756b5c45f5d
Leaf-WXC <523045770@qq.com>
2018-04-09 09:54:18 +0800

    完成所有四项功能

--- src/ButtonAppBar.js
+++ src/ButtonAppBar.js
@@ -10,7 +10,11 @@ import MenuIcon from 'material-ui-icons/Menu';
 import { MenuItem } from 'material-ui/Menu';
 import Drawer from 'material-ui/Drawer';
 
-const styles = {
+
+
+const drawerWidth = 240;
+
+const styles =  theme => ( {
   root: {
     flexGrow: 1,
   },
@@ -21,19 +25,53 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
-};
+  drawerPaper: {
+    width: drawerWidth
+  }
+
+});
+
+class Title1 extends Component{
+
+  render(){
+      return <main>我是谁？</main>
+  }
+}
+
+class Title2 extends Component{
+  render(){
+      return <main>我在哪？</main>
+  }
+}
+
+class Title3 extends Component{
+  render(){
+      return <main>我要做什么？</main>
+  }
+}
 
 class ButtonAppBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {
+      open: false,
+      open1:false,
+      open2: false,
+      open3: false
+    };
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
  
+  handleClick1 = () => this.setState({open1: !this.state.open1});
+
+  handleClick2 = () => this.setState({open2: !this.state.open2});
+
+  handleClick3 = () => this.setState({open3: !this.state.open3});
 
   render() {
   const { classes } =this.props;
+  if(!this.state.open1&&!this.state.open2&&!this.state.open3){
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -47,13 +85,28 @@ class ButtonAppBar extends Component {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-      <Drawer  open={this.state.open} onClose={this.handleToggle} >
-          <MenuItem> 功能1 </MenuItem>
-          <MenuItem> 功能2 </MenuItem>
-          <MenuItem> 功能3 </MenuItem>
+      <Drawer classes={{ paper: classes.drawerPaper}} open={this.state.open} onClose={this.handleToggle} >
+          <MenuItem onClick={this.handleClick1}> 功能1 </MenuItem>
+          <MenuItem onClick={this.handleClick2}> 功能2 </MenuItem>
+          <MenuItem onClick={this.handleClick3}> 功能3 </MenuItem>
         </Drawer>
     </div>
   );
+}
+else {
+  if(this.state.open1)
+{
+  return <Title1 /> ;
+}
+  if(this.state.open2)
+{
+  return <Title2 /> ;
+}
+  if(this.state.open3)
+{
+  return <Title3 /> ;
+}
+}
 }
 }
 
